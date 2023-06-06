0503fa7b2ac5ac7a65c654b32a84f88ba9f400ad
sementrof <114408546+sementrof@users.noreply.github.com>
2023-06-06 19:48:53 +0300

    Update test_copy_maker.sh

--- test_copy_maker.sh
+++ test_copy_maker.sh
@@ -10,12 +10,21 @@ fi
 
 
 
-./copy_maker.sh  1 <<< copy_filename
-if [ $# -eq 1 ];
+./copy_maker.sh 1 <<< copy_filename
+if [[ $? -eq 1 ]];
 then
     echo "тест 2 пройден"
 else
 echo " тест 2 не пройден"
+fi
+
+
+./copy_maker.sh filename.sh 3 <<< copy_filename
+if [[ $? -eq 1 ]];
+then
+    echo "тест 3 пройден"
+else
+    echo "тест 3 не пройден"
 fi
 
 
