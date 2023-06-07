3baac48f214d3bc8f6483ada7b02b2db18daa86a
sementrof <114408546+sementrof@users.noreply.github.com>
2023-06-07 13:59:29 +0300

    test3.sh

--- test3.sh
+++ test3.sh
@@ -2,9 +2,9 @@
 ./copy_maker.sh filename.sh 3 <<< copy_filename
 if [[ $? -eq 1 ]];
 then
-    echo "тест 3 не пройден"
-    exit 1
+    echo "тест 3 пройден"
+    exit 0
 else
     echo "тест 3 не пройден"
-    exit 0
+    exit 1
 fi
